eed1622f338d84c3c23b52f21dfcd1b2f5655425
Koustubh <36124868+babban-techie@users.noreply.github.com>
2020-04-15 22:35:00 +0530

    Add files via upload

---
+++ parking_sensor.ino
@@ -0,0 +1,33 @@
+#include <NewPing.h>
+ 
+#define TRIGGER_PIN  13
+#define ECHO_PIN     12
+#define MAX_DISTANCE 200
+#define RED 11
+#define GREEN 10
+ 
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+ 
+void setup() {
+  Serial.begin(115200);
+  pinMode(RED,OUTPUT);
+  pinMode(GREEN,OUTPUT);
+}
+ 
+void loop() {
+  int x = sonar.ping_cm();
+  delay(50);
+  if (x>20 || x==0)
+  {
+    digitalWrite(RED,LOW);
+    digitalWrite(GREEN,HIGH);
+  }
+  else
+  {
+    digitalWrite(GREEN,LOW);
+    digitalWrite(RED,HIGH);
+  }
+  Serial.print("Ping: ");
+  Serial.print(x);
+  Serial.println("cm");
+}
